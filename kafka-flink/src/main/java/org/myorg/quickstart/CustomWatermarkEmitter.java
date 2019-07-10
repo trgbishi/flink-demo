c74d07be475947d220c91faa3b3fafe11a79644f
yaobc <yaobc@ustc-it.com>
2019-07-10 16:04:33 +0800

    first commit

---
+++ kafka-flink/src/main/java/org/myorg/quickstart/CustomWatermarkEmitter.java
@@ -0,0 +1,27 @@
+package org.myorg.quickstart;
+
+import org.apache.flink.streaming.api.functions.AssignerWithPunctuatedWatermarks;
+import org.apache.flink.streaming.api.watermark.Watermark;
+
+public class CustomWatermarkEmitter implements AssignerWithPunctuatedWatermarks<String> {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public long extractTimestamp(String arg0, long arg1) {
+        if (null != arg0 && arg0.contains(",")) {
+            String parts[] = arg0.split(",");
+            return Long.parseLong(parts[0]);
+        }
+        return 0;
+    }
+
+    @Override
+    public Watermark checkAndGetNextWatermark(String arg0, long arg1) {
+        if (null != arg0 && arg0.contains(",")) {
+            String parts[] = arg0.split(",");
+            return new Watermark(Long.parseLong(parts[0]));
+        }
+        return null;
+    }
+}
---
+++ kafka-flink/src/main/java/org/myorg/quickstart/KafkaDemo.java
@@ -0,0 +1,56 @@
+package org.myorg.quickstart;
+
+
+import org.apache.flink.streaming.api.TimeCharacteristic;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+
+import java.util.Properties;
+
+
+/**
+ * 该类仅仅是一个kafka的连接测试
+ * 当指定了kafka端口及zookeeper端口 及 kafka topic
+ * 便能监听kafka该topic的数据
+ * 本demo将监听到的数据打印到控制台
+ */
+public class KafkaDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        // set up the streaming execution environment
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        //默认情况下，检查点被禁用。要启用检查点，请在StreamExecutionEnvironment上调用enableCheckpointing(n)方法，
+        // 其中n是以毫秒为单位的检查点间隔。每隔5000 ms进行启动一个检查点,则下一个检查点将在上一个检查点完成后5秒钟内启动
+        env.enableCheckpointing(5000);
+        env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+        Properties properties = new Properties();
+
+        properties.setProperty("bootstrap.servers", "10.0.99.102:9092");
+        properties.setProperty("zookeeper.connect", "10.0.99.102:2181,10.0.99.102:2182,10.0.99.102:2183");
+        properties.setProperty("group.id", "test-consumer-group");
+        FlinkKafkaConsumer<String> myConsumer = new FlinkKafkaConsumer<>("test-kafka-flink", new SimpleStringSchema(),
+                properties);
+        myConsumer.assignTimestampsAndWatermarks(new CustomWatermarkEmitter());
+        DataStream<String> keyedStream = env.addSource(myConsumer).filter((s)->{
+            int a = Integer.parseInt(s);
+            if(a>5) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        });
+//        keyedStream.
+        keyedStream.print();
+        // execute program
+        env.execute("Flink Streaming Java API Skeleton");
+
+    }
+}
+
+//nohup bin/kafka-server-start.sh config/server.properties > my_kafkaserver.log 2>&1 &
+//bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test-kafka-flink
+//bin/kafka-console-producer.sh --broker-list localhost:9092 --topic test-kafka-flink
